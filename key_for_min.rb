bc2454e94e94ef5f37a4422149a848dee55cedbe
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-31 22:37:13 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
     return
   end
   name_hash.each do |key, value|
-    if min_value.nil? || value <= min_value
+    if value <= min_value
       min_value = value
       min_key = key
     end
